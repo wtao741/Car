f824d5b3b79db970d220b496cacf8fa584092a92
wtao741 <1574675387@qq.com>
2015-07-17 18:05:27 +0800

    20150717 1805

--- src/com/wancheda/activity/CallActivity.java
+++ src/com/wancheda/activity/CallActivity.java
@@ -1,7 +1,16 @@
 package com.wancheda.activity;
 
+import android.os.Bundle;
+
 import com.wancheda.manager.BaseActivity;
 
 public class CallActivity extends BaseActivity {
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_call);
+	}
 }
