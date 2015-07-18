7462115ad70fa40bb4315909c4ae59936c070642
wtao741 <1574675387@qq.com>
2015-07-18 15:16:48 +0800

    20150718 wangtao 客服

--- src/com/wancheda/activity/CallActivity.java
+++ src/com/wancheda/activity/CallActivity.java
@@ -1,16 +1,64 @@
 package com.wancheda.activity;
 
+import net.tsz.afinal.annotation.view.ViewInject;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.wancheda.manager.BaseActivity;
 
 public class CallActivity extends BaseActivity {
 
+	@ViewInject(id = R.id.call_vip)
+	TextView tv_call_vip;
+	@ViewInject(id = R.id.call_south)
+	TextView tv_call_south;
+	@ViewInject(id = R.id.call_north)
+	TextView tv_call_north;
+	@ViewInject(id = R.id.call_cdu)
+	TextView tv_call_cdu;
+	@ViewInject(id = R.id.call)
+	ImageView iv_call;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		
+
 		setContentView(R.layout.activity_call);
+		
+		tv_call_vip.setOnClickListener(this);
+		tv_call_south.setOnClickListener(this);
+		tv_call_north.setOnClickListener(this);
+		tv_call_cdu.setOnClickListener(this);
+		iv_call.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		super.onClick(v);
+		String action = "tel:";
+		switch (v.getId()) {
+		case R.id.call:
+            action = action+"4000-050607";
+			break;
+		case R.id.call_vip:
+			 action = action+getResoure(R.string.call_vip_phone);
+			break;
+		case R.id.call_south:
+			 action = action+getResoure(R.string.call_south_phone);
+			break;
+		case R.id.call_north:
+			 action = action+getResoure(R.string.call_north_phone);
+			break;
+		case R.id.call_cdu:
+			 action = action+getResoure(R.string.call_cdu_phone);
+			break;
+		}
+		Intent intent = new Intent(Intent.ACTION_DIAL, Uri
+				.parse(action));
+		startActivity(intent);
 	}
 }
--- src/com/wancheda/manager/BaseActivity.java
+++ src/com/wancheda/manager/BaseActivity.java
@@ -1,8 +1,21 @@
 package com.wancheda.manager;
 
+import com.wancheda.activity.R;
+
+import android.view.View;
+import android.view.View.OnClickListener;
 import net.tsz.afinal.FinalActivity;
 
-public class BaseActivity extends FinalActivity{
+public class BaseActivity extends FinalActivity implements OnClickListener{
 
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getResoure(int id){
+		return getResources().getString(id);
+	}
 	
 }
